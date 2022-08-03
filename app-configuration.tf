e94507d4c79a27648319c9e5aff5fd254755ce33
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2022-08-03 12:48:57 +0200

    Imported app configuration

---
+++ app-configuration.tf
@@ -0,0 +1,11 @@
+﻿resource "azurerm_app_configuration" "app-config" {
+  name                = "appcs-noplan-dev"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "free"
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.id.id]
+  }
+  tags = var.tags
+}
